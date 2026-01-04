16f75f32469e61b5e9f105addaf46eedbadb737a
Ludovic <ludovicaubut@hotmail.com>
2026-01-03 22:01:36 -0500

    Added a missing parenthesis

--- sw.js
+++ sw.js
@@ -3,11 +3,14 @@ self.addEventListener("install", e => {
    e.waitUntil(
       caches.open("static").then(cache => {
          return cache.addAll(["./"]);
-      };
+      });
    );
    console.log("Finished: Service worker installation");
 });
 
 self.addEventListener("fetch", e => {
    console.log("SW: Intercepting request for: " + e.request.url);
+   e.respondWith(cache.match(e.request).then(response => {
+      return response || fetch(e.request);
+   }));
 });
