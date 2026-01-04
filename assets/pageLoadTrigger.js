eee42acfa16e1c365c9fca22d28dcd65bb828a7b
Ludovic <ludovicaubut@hotmail.com>
2026-01-03 22:00:10 -0500

    Added the foundation to making this a Progressive Web App (PWA)

--- assets/pageLoadTrigger.js
+++ assets/pageLoadTrigger.js
@@ -1,5 +1,19 @@
 var localeLoaded = false;
 
+if ("serviceWorker" in navigator) {
+   console.log("Started: Service worker registration");
+   navigator.serviceWorker.register("sw.js").then(registration => {
+      console.log("   Service Worker registered:\n" + registration);
+   }).catch(error => {
+      console.log("   Service Worker registration failed:\n" + error);
+   });
+   console.log("Finished: Service worker registration");
+} else {
+   let errorMessage = "Votre nagivateur ne supporte pas la navigation hors ligne pour cette application. Veuillez changer de navigateur.";
+   alert(errorMessage);
+   console.log(errorMessage);
+}
+
 document.addEventListener("DOMContentLoaded", async () => {
    console.log("Started: Locale retrieval");
    const browserLocalesArray = browserLocales(true);
---
+++ sw.js
@@ -0,0 +1,13 @@
+self.addEventListener("install", e => {
+   console.log("Started: Service worker installation");
+   e.waitUntil(
+      caches.open("static").then(cache => {
+         return cache.addAll(["./"]);
+      };
+   );
+   console.log("Finished: Service worker installation");
+});
+
+self.addEventListener("fetch", e => {
+   console.log("SW: Intercepting request for: " + e.request.url);
+});
