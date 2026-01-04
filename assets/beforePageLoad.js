389e5ae04b3f5bfc08a4e701d4df19870837ea4b
Ludovic <ludovicaubut@hotmail.com>
2026-01-04 03:04:16 -0500

    Displacing a log so it is displayed in order with other logs

--- assets/beforePageLoad.js
+++ assets/beforePageLoad.js
@@ -1,11 +1,12 @@
 if ("serviceWorker" in navigator) {
    window.addEventListener('load' , () => {
       console.log("Started: Service worker registration");
-      navigator.serviceWorker.register("./sw.js").then(registration => {
-         console.log("   Service Worker registered with scope: " + registration.scope);
+      let result = await navigator.serviceWorker.register("./sw.js").then(registration => {
+         return "   Service Worker registered with scope: " + registration.scope;
       }).catch(error => {
-         console.log("   Service Worker registration failed:\n   " + error);
+         return "   Service Worker registration failed:\n   " + error;
       });
+      console.log(result);
       console.log("Finished: Service worker registration");
    });
 } else {
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "tradify-v32";
+const CACHE_NAME = "tradify-v33";
 const APP_STATIC_RESOURCES = [
    "./",
    "./index.html",
