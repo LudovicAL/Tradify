57fad0f64201b5dabe8a574dbf2546890aa88b2a
Ludovic <ludovicaubut@hotmail.com>
2026-01-04 03:07:38 -0500

    Moving the service worker registration in an async function

--- assets/beforePageLoad.js
+++ assets/beforePageLoad.js
@@ -1,11 +1,7 @@
 if ("serviceWorker" in navigator) {
    window.addEventListener('load' , () => {
       console.log("Started: Service worker registration");
-      let result = await navigator.serviceWorker.register("./sw.js").then(registration => {
-         return "   Service Worker registered with scope: " + registration.scope;
-      }).catch(error => {
-         return "   Service Worker registration failed:\n   " + error;
-      });
+      let result = registerServiceWorker();
       console.log(result);
       console.log("Finished: Service worker registration");
    });
@@ -13,4 +9,12 @@ if ("serviceWorker" in navigator) {
    let errorMessage = "Votre nagivateur ne supporte pas la navigation hors ligne pour cette application. Veuillez changer de navigateur.";
    alert(errorMessage);
    console.log(errorMessage);
+}
+
+async function registerServiceWorker() {
+   await navigator.serviceWorker.register("./sw.js").then(registration => {
+         return "   Service Worker registered with scope: " + registration.scope;
+      }).catch(error => {
+         return "   Service Worker registration failed:\n   " + error;
+      });
 }
