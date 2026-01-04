9dc6612ef4edce373258f588f74c270a809dd9bb
Ludovic <ludovicaubut@hotmail.com>
2026-01-04 01:11:37 -0500

    Temporarily removing cache function that broke the app

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "tradify-v15";
+const CACHE_NAME = "tradify-v16";
 const APP_STATIC_RESOURCES = [
    "/index.html",
    "/icons/Title.png",
@@ -29,7 +29,7 @@ self.addEventListener("install", (installEvent) => {
    installEvent.waitUntil(
       (async () => {
          const cache = await caches.open(CACHE_NAME);
-         cache.addAll(APP_STATIC_RESOURCES);
+         //cache.addAll(APP_STATIC_RESOURCES);
       })()
    );
    console.log("Finished: Service worker installation");
@@ -66,7 +66,7 @@ self.addEventListener("fetch", fetchEvent => {
          }
          console.log("SW: Querying server for: " + fetchEvent.request.url);
          const responseFromNetwork = await fetch(fetchEvent.request);
-         await cache.put(request, response);
+         //await cache.put(request, response);
          return responseFromNetwork;
       })() 
    );
