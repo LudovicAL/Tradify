13c6928f90c4fc9d68c38595080f63d205917bf3
Ludovic <ludovicaubut@hotmail.com>
2026-01-18 00:51:55 -0500

    Adding missing meta tags

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "tradify-v54";
+const CACHE_NAME = "tradify-v55";
 const APP_STATIC_RESOURCES = [
    "./assets/aho-corasick/aho-corasick.js",
    "./assets/als-wave-parser/wav-parser.js",
